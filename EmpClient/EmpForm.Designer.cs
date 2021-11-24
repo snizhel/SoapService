317a3127020ed22b153932f6828e7326ffdacf66
Your Name <you@example.com>
2021-11-24 18:20:36 +0700

    soap

---
+++ EmpClient/EmpForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace EmpClient
+{
+    partial class EmpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdEmp = new System.Windows.Forms.DataGridView();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdEmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdEmp
+            // 
+            this.grdEmp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdEmp.Location = new System.Drawing.Point(137, 72);
+            this.grdEmp.Name = "grdEmp";
+            this.grdEmp.RowHeadersWidth = 51;
+            this.grdEmp.RowTemplate.Height = 24;
+            this.grdEmp.Size = new System.Drawing.Size(511, 150);
+            this.grdEmp.TabIndex = 0;
+            this.grdEmp.SelectionChanged += new System.EventHandler(this.grdEmp_SelectionChanged);
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(137, 13);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(443, 22);
+            this.txtKeyword.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(586, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Keyword";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(137, 257);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(449, 22);
+            this.txtID.TabIndex = 4;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(137, 328);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(449, 22);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(137, 365);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(449, 22);
+            this.txtSalary.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(137, 288);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(449, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(137, 403);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(449, 22);
+            this.txtAge.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Salary";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 409);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Age";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(621, 256);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(621, 303);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(621, 348);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // EmpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.grdEmp);
+            this.Name = "EmpForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.EmpForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdEmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdEmp;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ EmpClient/EmpForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpClient
+{
+    public partial class EmpForm : Form
+    {
+        bsite.EmployService employService=new bsite.EmployService();
+        public EmpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EmpForm_Load(object sender, EventArgs e)
+        {
+            List<bsite.Employee> employees=employService.GetAll().ToList();
+            grdEmp.DataSource = employees;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            String keyword=txtKeyword.Text.Trim();
+            List<bsite.Employee> employees = employService.Search(keyword).ToList();
+            grdEmp.DataSource = employees;
+        }
+
+        private void grdEmp_SelectionChanged(object sender, EventArgs e)
+        {
+            if (grdEmp.SelectedRows.Count == 1)
+            {
+                int id = int.Parse(grdEmp.SelectedRows[0].Cells["ID"].Value.ToString());
+                bsite.Employee employee = employService.GetEmployee(id);
+                if (employee!=null)
+                {
+                    txtID.Text = employee.ID.ToString();
+                    txtName.Text = employee.Name;
+                    txtAddress.Text = employee.Address;
+                    txtSalary.Text = employee.Salary.ToString();
+                    txtAge.Text = employee.Age.ToString();
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bsite.Employee employee = new bsite.Employee()
+            {
+                ID = 0,
+                Name = txtName.Text.Trim(),
+                Address = txtAddress.Text.Trim(),
+                Salary = int.Parse(txtSalary.Text.Trim()),
+                Age = int.Parse(txtAge.Text.Trim()),
+            };
+            bool result = employService.Add(employee);
+            if (result)
+            {
+                List<bsite.Employee> employees = employService.GetAll().ToList();
+                grdEmp.DataSource = employees;
+            }
+            else
+            {
+                MessageBox.Show("SORRY HONEY");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            bsite.Employee employee = new bsite.Employee()
+            {
+                ID = int.Parse(txtID.Text.Trim()),
+                Name = txtName.Text.Trim(),
+                Address = txtAddress.Text.Trim(),
+                Salary = int.Parse(txtSalary.Text.Trim()),
+                Age = int.Parse(txtAge.Text.Trim()),
+            };
+            bool result = employService.Update(employee);
+            if (result)
+            {
+                List<bsite.Employee> employees = employService.GetAll().ToList();
+                grdEmp.DataSource = employees;
+            }
+            else
+            {
+                MessageBox.Show("SORRY HONEY");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("ARE U SURE?", "CONFIRMATION", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                int id = int.Parse(txtID.Text.Trim());
+                bool result = employService.Delete(id);
+                if (result)
+                {
+                    List<bsite.Employee> employees = employService.GetAll().ToList();
+                    grdEmp.DataSource = employees;
+                }
+                else
+                {
+                    MessageBox.Show("SORRY HONEY");
+                }
+            }
+        }
+    }
+}
